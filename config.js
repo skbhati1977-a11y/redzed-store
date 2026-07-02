13ff29408b1a05807691fb9e939ec249ed8c0ae2
skbhati1977-a11y <skbhati1977@gmail.com>
2026-07-02 12:07:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,7 @@ const SUPABASE_URL = "PASTE_SUPABASE_PROJECT_URL_HERE";
 const SUPABASE_ANON_KEY = "PASTE_SUPABASE_ANON_KEY_HERE";
 const WHATSAPP_NUMBER = "9654401954";
 const ADMIN_PIN = "9654"; // बाद में बदल दें
+const CFG = {
+  ADMIN_PIN: ADMIN_PIN
+};
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
