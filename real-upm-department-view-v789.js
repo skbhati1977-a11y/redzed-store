725eaeff55399d58704aba9d1eed2e99e398a0fa
skbhati1977-a11y <skbhati1977@gmail.com>
2026-08-15 19:19:23 +0530

    Load dedicated ALTER journey overlay V9113

--- real-upm-department-view-v789.js
+++ real-upm-department-view-v789.js
@@ -1,5 +1,5 @@
 (() => {
   'use strict';
   const load=(src,next)=>{const s=document.createElement('script');s.src=src;s.onload=()=>next&&next();s.onerror=()=>console.error('REAL FACTORY loader failed',src);document.head.appendChild(s)};
-  load('real-upm-department-view-v789-core-v9110.js?v=9112',()=>load('real-upm-worker-request-v9112.js?v=9112'));
+  load('real-upm-department-view-v789-core-v9110.js?v=9112',()=>load('real-upm-worker-request-v9112.js?v=9112',()=>load('real-upm-alter-flow-v9113.js?v=9113')));
 })();
